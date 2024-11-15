aa417cb1c4c61562d6cd3bb2a882110fa35c83a7
NickH880 <NicholasNPH220@gmail.com>
2024-11-15 13:11:40 -0500

    Add files via upload

---
+++ Lane Counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lane_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            int currentLane = 1; // Start at lane 1
+            Console.WriteLine("Press the spacebar."); //Holding also increases it, kinda cool
+            Console.WriteLine("Press 'Esc' to exit.");
+
+            while (true)
+            {
+                if (Console.KeyAvailable) //For the check
+                {
+                    var key = Console.ReadKey(intercept: true).Key; //To make sure spacebar
+
+                    if (key == ConsoleKey.Spacebar) //For the spacebar specifically
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Current Lane: " + currentLane); 
+                        currentLane++; //Increase lane
+                    }
+                    else if (key == ConsoleKey.Escape) //For an exit
+                    {
+                        break; //End
+                    }
+                }
+            }
+        }
+    }
+}
